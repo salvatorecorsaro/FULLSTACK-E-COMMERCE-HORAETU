a9de3eada113fa61d69ee3e5d3d7e8aeade9ac5c
Salvatore Corsaro <55556636+salvatorecorsaro@users.noreply.github.com>
2021-03-15 13:12:09 +0100

    Add files via upload

---
+++ only-git.sh
@@ -0,0 +1,18 @@
+echo "Setting up project: Horaetu"
+
+git clone https://github.com/salvatorecorsaro/eureka-HORAE horaetu/eureka
+
+git clone https://github.com/salvatorecorsaro/catalogue-HORAE horaetu/catalogue
+
+git clone https://github.com/salvatorecorsaro/geo-HORAE horaetu/geo
+
+git clone https://github.com/salvatorecorsaro/shop-HORAE horaetu/shop
+git clone https://github.com/salvatorecorsaro/chatbot-HORAE horaetu/chatbot
+
+git clone https://github.com/salvatorecorsaro/front_HORAETU horaetu/front
+
+git clone https://github.com/salvatorecorsaro/gateway-HORAE horaetu/gateway
+
+
+
+echo "Horatu downloaded"
